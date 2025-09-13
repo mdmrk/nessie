7b87bda7def3adc13c954c6fc74f54ef83d6ac1e
mdmrk <mariodavo.20@gmail.com>
2025-09-13 21:49:25 +0200

    egui window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+struct App {}
+
+impl App {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("test");
+        });
+    }
+}
+
+fn main() -> eframe::Result {
+    env_logger::init();
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([1280.0, 720.0]),
+        ..Default::default()
+    };
+    eframe::run_native("Nessie", options, Box::new(|_cc| Ok(Box::new(App::new()))))
 }
