5a93fac0f78e41153de0334ae22da6fb39009bc0
mdmrk <mariodavo.20@gmail.com>
2025-11-26 12:16:17 +0100

    try to fix cache on wasm

--- assets/sw.js
+++ assets/sw.js
@@ -1,29 +1,14 @@
-var cacheName = "nessie-pwa-" + (typeof __BUILD_ID__ !== 'undefined' ? __BUILD_ID__ : 'development');
+var cacheName = 'nessie-pwa';
 var filesToCache = ["./", "./index.html", "./nessie.js", "./nessie_bg.wasm"];
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    }),
-  );
+self.addEventListener("install", function(e) {
+	e.waitUntil(caches.open(cacheName).then(function(cache) {
+		return cache.addAll(filesToCache);
+	}), );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name.startsWith('nessie-pwa-') && name !== cacheName)
-                  .map(name => caches.delete(name))
-      );
-    })
-  );
-});
-
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    }),
-  );
+self.addEventListener("fetch", function(e) {
+	e.respondWith(caches.match(e.request).then(function(response) {
+		return response || fetch(e.request);
+	}), );
 });
